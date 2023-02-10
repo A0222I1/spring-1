e4db7434f2fd9a1a8d7261048fe6569d8f502555
huuphucdn94 <huuphucdn94@gmail.com>
2023-02-10 16:33:56 +0700

    update 10-2

--- Angular_1/angular-building/src/app/app-routing.module.ts
+++ Angular_1/angular-building/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {EmployeeModuleModule} from "./module/employee-module/employee-module.module";
+import {CustomerModuleComponent} from "./module/customer-module/customer-module.component";
 
 
 const routes: Routes = [
@@ -9,7 +10,10 @@ const routes: Routes = [
 },
   {
   path: "employee", component: EmployeeModuleModule
-}
+},
+  {
+    path: "customer", component: CustomerModuleComponent
+  }
 ];
 
 @NgModule({
