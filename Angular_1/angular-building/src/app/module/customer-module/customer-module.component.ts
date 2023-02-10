a647a256cc1bf6b868aba5513dbb200b7cc820e4
huuphucdn94 <huuphucdn94@gmail.com>
2023-02-10 16:20:15 +0700

    update service and component

--- Angular_1/angular-building/src/app/module/customer-module/customer-module.component.ts
+++ Angular_1/angular-building/src/app/module/customer-module/customer-module.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Customer} from "./model/Customer";
+import {CustomerServiceService} from "./service/customer-service.service";
+import {CustomerViewDTO} from "./dto/CustomerViewDTO";
 
 @Component({
   selector: 'app-customer-module',
@@ -8,11 +11,66 @@ import {FormGroup} from "@angular/forms";
 })
 export class CustomerModuleComponent implements OnInit {
   formGroup: FormGroup;
+  customers: CustomerViewDTO[] = [];
+  totalPages: number = 0;
+  pageNumber: number = 0;
+  name_search: string = '';
+  cmnd_search: string = '';
+  fileChose: File = null;
+  searchForm: boolean = false;
+  message: string = '';
+  alert: boolean = false;
 
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private customerService: CustomerServiceService,
+              private formBuilder: FormBuilder) {
+    this.buildForm();
+    this.findAllWithCondition(this.name_search, this.cmnd_search,0);
+    setTimeout(() => this.alert = false, 3000);
   }
 
+  ngOnInit(): void {
+    this.buildForm();
+    this.findAllWithCondition(this.name_search, this.cmnd_search, 0);
+    setTimeout(() => this.alert = false, 3000);
+  }
+
+
+
+  private findAllWithCondition(name: string, id_card: string, page: number) {
+    if (page > this.totalPages) return;
+    this.customerService.findAllByNameAndIdCard(name, id_card, page).subscribe(value => {
+      this.customers = value.content
+      this.pageNumber = value.number;
+      this.totalPages = value.totalPages;
+    });
+  }
+  refreshPage(){
+    (<HTMLInputElement>document.getElementById("nameSearch")).value = '';
+    (<HTMLInputElement>document.getElementById("cmndSearch")).value = '';
+    this.cmnd_search = '';
+    this.name_search = '';
+    this.ngOnInit();
+  }
+  deleteAll(id: string){
+    this.customerService.updateStatusById(id).subscribe(value => {
+      this.message = `xóa khách hàng với id ${id} thành công!!!`
+      this.alert = true;
+      document.getElementById("deleteModal").click();
+      this.ngOnInit();
+    })
+  }
+  findById(id: string){
+    this.customerService.findById(id).subscribe(value => {
+      document.getElementById("name_delete").innerText =value.name;
+        (<HTMLInputElement>document.getElementById("id_delete")).value = value.id;
+    },
+      error => {
+        this.message = error.error;
+        this.alert = true;
+        this.ngOnInit();
+      })
+  }
+  private buildForm() {
+    //validator
+  }
 }
--- Angular_1/angular-building/src/app/module/customer-module/service/customer-service.service.ts
+++ Angular_1/angular-building/src/app/module/customer-module/service/customer-service.service.ts
@@ -17,6 +17,15 @@ export class CustomerServiceService {
   findAllByNameAndIdCard(name: string, id_card: string, page: number): Observable<GetResponse>{
     return this.http.get<GetResponse>(`${this.url}?name=${name}&id_card=${id_card}&page=${page}`);
   }
+  updateAllStatusIsOff(): Observable<number> {
+    return this.http.delete<number>(`${this.url}`);
+  }
+  updateStatusById(id: string): Observable<number> {
+    return this.http.delete<number>(`${this.url}/${id}`);
+  }
+  findById(id: string): Observable<Customer>{
+    return this.http.get<Customer>(`${this.url}/${id}`)
+  }
 
   findByIdCard(value: string): Observable<boolean>{
     return this.http.get<boolean>(`${this.url}/existsIdCard?id_card=/${value}`);
@@ -24,17 +33,29 @@ export class CustomerServiceService {
   findByName(value: string): Observable<boolean>{
     return this.http.get<boolean>(`http://localhost:8080/account/${value}`);
   }
-  // save(value: AbstractControl): Observable<Customer>{
-  //   return this.http.post<Customer>(`${this.url}`, this.getCustomerDTO(value));
-  // }
-  //
-  // private getCustomerDTO(control: AbstractControl): CustomerFormCreateDTO {
-  //   return null;
-  // }
+  save(value: AbstractControl): Observable<Customer>{
+    return this.http.post<Customer>(`${this.url}`, this.getCustomerDTO(value));
+  }
+
+  private getCustomerDTO(control: AbstractControl): CustomerFormCreateDTO {
+    return {
+      idCard: control.value.idCard,
+      name: control.value.name.trim(),
+      email: control.value.email,
+      phone: control.value.phone,
+      birthday: control.value.birthday,
+      address: control.value.address,
+      website: control.value.address,
+      company: control.value.company,
+      avatar: control.value.avatar,
+      gender: control.value.gender,
+      account: control.value.account
+    }
+  }
 }
 
 interface GetResponse {
-  content: CustomerViewDTO;
+  content: CustomerViewDTO[];
   totalPages: number;
   number: number;
 }
