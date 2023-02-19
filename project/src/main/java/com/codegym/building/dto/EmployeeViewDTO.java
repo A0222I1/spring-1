48b6d0a81e75458e1618f47964763e9aba246083
Le Ngoc Tuan <cumeo154@gmail.com>
2023-02-19 21:35:09 +0700

    TuanLN6 fix bug delete jump in current page

--- project/src/main/java/com/codegym/building/dto/EmployeeViewDTO.java
+++ project/src/main/java/com/codegym/building/dto/EmployeeViewDTO.java
@@ -1,5 +1,7 @@
 package com.codegym.building.dto;
 
+import com.codegym.building.model.account.AccountRole;
+import com.codegym.building.model.account.Roles;
 import com.codegym.building.model.person.Employee;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,6 +10,8 @@ import lombok.Setter;
 
 import java.sql.Date;
 import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -28,8 +32,9 @@ public class EmployeeViewDTO {
     String department;
     String account;
     String dateBegin;
+    Long maxRole;
 
-    public EmployeeViewDTO(Employee employee){
+    public EmployeeViewDTO(Employee employee) {
         this.id = employee.getId();
         this.avatar = employee.getAvatar();
         this.name = employee.getName();
@@ -43,6 +48,15 @@ public class EmployeeViewDTO {
         this.salaryScale = employee.getSalaryScale().getName();
         this.department = employee.getDepartment().getName();
         this.account = employee.getAccount() == null ? "chưa có tài khoản" : employee.getAccount().getUser_name();
-        this.dateBegin = employee.getAccount() == null ? "chưa có tài khoản" : new SimpleDateFormat("dd/MM/yyyy").format(employee.getAccount().getDateCreate()) ;
+        this.dateBegin = employee.getAccount() == null ? "chưa có tài khoản" : new SimpleDateFormat("dd/MM/yyyy").format(employee.getAccount().getDateCreate());
+        this.maxRole = getMaxRole(employee.getAccount().getAccountRoles());
+    }
+
+    public Long getMaxRole(List<AccountRole> listAccountRole) {
+        Roles roles = listAccountRole.stream().map(AccountRole::getRoles).min(Comparator.comparing(Roles::getId)).orElse(null);
+        if (roles == null) {
+            return 0L;
+        }
+        return roles.getId();
     }
 }
