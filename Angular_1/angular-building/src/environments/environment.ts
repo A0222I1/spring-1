f0440377c334f491a327e79f5f42f9fafdf52e78
doleluan <leluando2001@gmail.com>
2023-02-22 20:00:21 +0700

    LUANDL update code firebase

--- Angular_1/angular-building/src/environments/environment.ts
+++ Angular_1/angular-building/src/environments/environment.ts
@@ -4,16 +4,30 @@
 
 export const environment = {
   production: false,
+  //LUANDL
+  // firebaseConfig : {
+  //   // config của project
+  //   apiKey: "AIzaSyAbMuVCgTmCrzoDCN4WFPW1vaF23TvdOvQ",
+  //   authDomain: "fire-angular-41bd5.firebaseapp.com",
+  //   projectId: "fire-angular-41bd5",
+  //   storageBucket: "fire-angular-41bd5.appspot.com",
+  //   messagingSenderId: "335778822886",
+  //   appId: "1:335778822886:web:d14e966ca4631d559b9927",
+  //   // địa chỉ database
+  //   databaseURL: "gs://fire-angular-41bd5.appspot.com"
+  // }
+  //TUANLN
   firebaseConfig : {
     // config của project
-    apiKey: "AIzaSyAbMuVCgTmCrzoDCN4WFPW1vaF23TvdOvQ",
-    authDomain: "fire-angular-41bd5.firebaseapp.com",
-    projectId: "fire-angular-41bd5",
-    storageBucket: "fire-angular-41bd5.appspot.com",
-    messagingSenderId: "335778822886",
-    appId: "1:335778822886:web:d14e966ca4631d559b9927",
+    apiKey: "AIzaSyD_GZCTGxqPd756XV12-bSlvwqXBAhcx5k",
+    authDomain: "projectspring1-3a404.firebaseapp.com",
+    projectId: "projectspring1-3a404",
+    storageBucket: "projectspring1-3a404.appspot.com",
+    messagingSenderId: "139714467302",
+    appId: "1:139714467302:web:7f88912ff48a18839f3227",
+    measurementId: "G-1FD5Y69Z9H",
     // địa chỉ database
-    databaseURL: "gs://fire-angular-41bd5.appspot.com"
+    databaseURL: "gs://projectspring1-3a404.appspot.com"
   }
 };
 
