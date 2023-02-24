1f2fc09113d15c4c52efac1927a4b232a3af4e68
LinhPhan <linhdhpd02123@fpt.edu.vn>
2023-02-24 18:07:07 +0700

    LinhPNT update code

--- Angular_1/angular-building/src/app/module/static-module/validate/validate.ts
+++ Angular_1/angular-building/src/app/module/static-module/validate/validate.ts
@@ -7,9 +7,25 @@ export function checkDate(controls: AbstractControl): ValidationErrors | null {
 
   let startDate = moment(new Date(controls?.parent?.get('startDate')?.value));
   let finalDate = moment(new Date(controls?.parent?.get('finalDate')?.value));
-  console.log("startDate: " + startDate);
-  console.log("finalDate: " + finalDate);
-  console.log("res: " + (startDate <= finalDate));
+  console.log("aaaa"+ controls?.parent?.get('startDate')?.value);
 
-  return (startDate <= finalDate) ? null : {invalidDate: true};
+  return ('' == controls?.parent?.get('startDate')?.value) || (startDate <= finalDate) ? null : {invalidDate: true};
+}
+
+export function checkDateLow(controls: AbstractControl): ValidationErrors | null {
+  console.log(controls);
+  const check = controls.value;
+
+  let startDate = moment(new Date(controls?.parent?.get('startLowDate')?.value));
+  let finalDate = moment(new Date(controls?.parent?.get('finalLowDate')?.value));
+  return ('' == controls?.parent?.get('startLowDate')?.value) || (startDate <= finalDate) ? null : {invalidDate: true};
+}
+
+export function checkDateHigh(controls: AbstractControl): ValidationErrors | null {
+  console.log(controls);
+  const check = controls.value;
+
+  let startDate = moment(new Date(controls?.parent?.get('startHighDate')?.value));
+  let finalDate = moment(new Date(controls?.parent?.get('finalHighDate')?.value));
+  return ('' == controls?.parent?.get('startHighDate')?.value) || (startDate <= finalDate) ? null : {invalidDate: true};
 }
